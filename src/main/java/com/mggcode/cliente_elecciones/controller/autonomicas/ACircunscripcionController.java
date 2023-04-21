f0912b42c0788ffa5218896dd5d3d53cd64a7004
Mohamed <moha.as172@gmail.com>
2023-04-21 10:35:46 +0200

    eliminado websocket

--- src/main/java/com/mggcode/cliente_elecciones/controller/autonomicas/ACircunscripcionController.java
+++ src/main/java/com/mggcode/cliente_elecciones/controller/autonomicas/ACircunscripcionController.java
@@ -1,7 +1,6 @@
 package com.mggcode.cliente_elecciones.controller.autonomicas;
 
 
-import com.mggcode.cliente_elecciones.conexion.WebSocketSessionHandler;
 import com.mggcode.cliente_elecciones.data.Data;
 import com.mggcode.cliente_elecciones.model.Circunscripcion;
 import com.mggcode.cliente_elecciones.service.autonomicas.ACarmenDTOService;
@@ -9,8 +8,6 @@ import com.mggcode.cliente_elecciones.service.autonomicas.ACircunscripcionServic
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.converter.StringMessageConverter;
-import org.springframework.messaging.simp.stomp.StompSessionHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.socket.client.WebSocketClient;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -181,15 +175,5 @@ public class ACircunscripcionController {
         return changes.contains(circunscripcionService.findById(codigo));
     }
 
-    @GetMapping("/updates")
-    ResponseEntity<String> subscribe() {
-        String url = "ws://localhost:8080/autonomicas/updates";
-        WebSocketClient client = new StandardWebSocketClient();
-        WebSocketStompClient stompClient = new WebSocketStompClient(client);
-        stompClient.setMessageConverter(new StringMessageConverter());
 
-        StompSessionHandler sessionHandler = new WebSocketSessionHandler();
-        var res = stompClient.connectAsync(url, sessionHandler);
-        return new ResponseEntity<>("Conectado", HttpStatus.OK);
-    }
 }
